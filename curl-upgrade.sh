7edb52250b21ee78759208f0aa56afb833f5c8f0
Robin Wen <blockxyz@gmail.com>
2023-10-12 14:11:12 +0800

    add ver parameter

--- curl-upgrade.sh
+++ curl-upgrade.sh
@@ -9,19 +9,21 @@
 # Date: 2023-10-12
 # Time: 11:44:49
 
+ver="8.4.0"
+
 # install dependencies
 sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev
 
 # build curl from source
-cd ~
-wget https://curl.se/download/curl-8.4.0.tar.gz
-tar -zxvf curl-8.4.0.tar.gz
-cd curl-8.4.0 && ./configure --with-ssl && make && sudo make install
+cd /home/ubuntu
+wget https://curl.se/download/curl-$ver.tar.gz
+tar -zxvf curl-$ver.tar.gz
+cd curl-$ver && ./configure --with-ssl && make && sudo make install
 sudo ldconfig
 
 # remove source file
-cd ~ && rm -rf curl-8.4.0 curl-8.4.0.tar.gz
+cd /home/ubuntu && rm -rf curl-$ver curl-$ver.tar.gz
 sudo rm -f /usr/bin/curl
 
 if [ -f "/usr/local/bin/curl" ]; then
@@ -33,7 +35,7 @@ which curl
 curl --version
 
 # remove upgrade script
-count=`curl --version | grep -w 8.4.0 | wc -l`
+count=`curl --version | grep -w $ver | wc -l`
 if [ $count -eq 1 ]; then
-    cd ~ && rm -f curl-upgrade.sh
+    cd /home/ubuntu && rm -f curl-upgrade.sh
 fi
